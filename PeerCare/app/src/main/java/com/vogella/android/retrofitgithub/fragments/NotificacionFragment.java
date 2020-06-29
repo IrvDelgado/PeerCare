de8337e85c78ba7bf2d1e3cb9a44b538289c3102
Irving <irvingdelgado6@gmail.com>
2020-06-28 23:06:42 -0500

    Listviews

--- PeerCare/app/src/main/java/com/vogella/android/retrofitgithub/fragments/NotificacionFragment.java
+++ PeerCare/app/src/main/java/com/vogella/android/retrofitgithub/fragments/NotificacionFragment.java
@@ -1,10 +1,13 @@
 package com.vogella.android.retrofitgithub.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.vogella.android.retrofitgithub.R;
 
@@ -12,7 +15,11 @@ import com.vogella.android.retrofitgithub.R;
  * A simple {@link Fragment} subclass.
  */
 public class NotificacionFragment extends Fragment {
-
+    private ListView lv1;
+    private String notificaciones []= {
+            "Usuario terminó tu tarea",
+            "Usuario solicitó hacer tu tarea",
+            "Usuario rechazó tu tarea"};
     public NotificacionFragment() {
         // Required empty public constructor
     }
@@ -23,5 +30,14 @@ public class NotificacionFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notificacion, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        lv1 = (ListView) view.findViewById(R.id.listview1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_personalizado,notificaciones);
+        lv1.setAdapter(adapter);
+
     }
 }
--- PeerCare/app/src/main/java/com/vogella/android/retrofitgithub/fragments/tareasaceptadas.java
+++ PeerCare/app/src/main/java/com/vogella/android/retrofitgithub/fragments/tareasaceptadas.java
@@ -1,10 +1,13 @@
 package com.vogella.android.retrofitgithub.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.vogella.android.retrofitgithub.R;
 
@@ -12,7 +15,11 @@ import com.vogella.android.retrofitgithub.R;
  * A simple {@link Fragment} subclass.
  */
 public class tareasaceptadas extends Fragment {
-
+    private ListView lv1;
+    private String notificaciones []= {
+            "Limpiar el sótano",
+            "Barrer el patio",
+            "Pasear al perro"};
     public tareasaceptadas() {
         // Required empty public constructor
     }
@@ -24,4 +31,16 @@ public class tareasaceptadas extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tareasaceptadas, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        lv1 = (ListView) view.findViewById(R.id.listview3);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_personalizado,notificaciones);
+        lv1.setAdapter(adapter);
+
+    }
+
+
+
 }
